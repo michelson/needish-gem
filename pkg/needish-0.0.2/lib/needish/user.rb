164fb7d4a8d24679337a61fb806d45d94c351573
miguel michelson martinez <miguelmichelson@gmail.com>
2008-07-07 17:32:17 -0400

    change to 0.0.2 , The apikey passes to the method initialize, now is not a global variable, (security issues)

---
+++ pkg/needish-0.0.2/lib/needish/user.rb
@@ -0,0 +1,29 @@
+module Needish
+  class User
+    include EasyClassMaker
+       
+   attributes :id,:name,:lastname,:nickname,:pictureurl,:displayname,:biography,
+   :userneeds
+   
+    
+    class << self
+      # Creates a new user from a piece of xml
+      def new_from_xml(xml)
+        User.new do |u|
+       
+        
+          u.id                           = xml.search("user").first[:id]
+          u.name                         = xml.search("user").first[:name]
+          u.lastname                     = xml.search("user").first[:lastname]
+          u.nickname                   = xml.search("user").first[:nickname]
+          u.pictureurl                   = xml.search("user").first[:pictureurl]
+          u.displayname                  = xml.search("user").first[:displayname]
+          u.biography                   = xml.search('biobraphy').inner_html
+          
+          
+       
+        end
+      end
+    end
+  end
+end
